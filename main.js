733e00426e6835e7926953942485f153930db444
Toka Abdelrhman <148630612+tokaAbdelrhman@users.noreply.github.com>
2024-05-08 23:31:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let image=document.querySelector(".image");
+function phones(phone){
+    image.src =phone;
+}
+let x=document.querySelector(".contain");
+function changeColor(color){
+    
+    x.style.background=color;
+}
